ed93b95c48805ae5c458f98be1b167bfabdc6a36
TAHEP <tansiner@hotmail.com>
2023-02-24 13:48:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -128,51 +128,62 @@ class Users(db.Model):
 @app.route('/')
 @app.route('/home')
 def home():
-    main_slider = Media
-    print(main_slider)
+    # main_slider = Media
+    # print(main_slider)
     
-    main_slider = Media
-    ms_slides = []
-    for slide in main_slider.query.filter_by().all():
-        ms_slides.append(slide)
+    # main_slider = Media
+    # ms_slides = []
+    # for slide in main_slider.query.filter_by().all():
+    #     ms_slides.append(slide)
 
-    print(ms_slides)
+    # print(ms_slides)
+
+
+    users = Users
+    my_users = []
+    for slide in users.query.filter_by().all():
+        my_users.append(slide)
+
+    print(my_users)
 
 
 
-    series = Media
-    series_slides = []
-    for slide in series.query.filter_by(category="series", carousel="1").all():
+    return "hello"
+
+
+        # series = Media
+        # series_slides = []
+        # for slide in series.query.filter_by(category="series", carousel="1").all():
+        
+        #     series_slides.append(slide)
+
+
+        # documentaries = Media
+        # documentaries_slides = []
+        # for slide in documentaries.query.filter_by(category="documentary", carousel="1").all():
+        
+        #     documentaries_slides.append(slide)
+
+
+        # movies = Media
+        # movies_slides = []
+        # for slide in movies.query.filter_by(category="movie", carousel="1").all():
+        
+        #     movies_slides.append(slide)
+
+
+        # tvshows = Media
+        # tvshows_slides = []
+        # for slide in tvshows.query.filter_by(category="tvshow", carousel="1").all():
+        
+        #     tvshows_slides.append(slide)
+
     
-        series_slides.append(slide)
-
-
-    documentaries = Media
-    documentaries_slides = []
-    for slide in documentaries.query.filter_by(category="documentary", carousel="1").all():
-    
-        documentaries_slides.append(slide)
-
-
-    movies = Media
-    movies_slides = []
-    for slide in movies.query.filter_by(category="movie", carousel="1").all():
-    
-        movies_slides.append(slide)
-
-
-    tvshows = Media
-    tvshows_slides = []
-    for slide in tvshows.query.filter_by(category="tvshow", carousel="1").all():
-    
-        tvshows_slides.append(slide)
-
-
-    return render_template("home.html", ms_slides = ms_slides,
-                                        series_slides = series_slides,
-                                        documentaries_slides = documentaries_slides, 
-                                        movies_slides = movies_slides,
-                                        tvshows_slides = tvshows_slides)
+        # return render_template("home.html", ms_slides = ms_slides,
+        #                                     series_slides = series_slides,
+        #                                     documentaries_slides = documentaries_slides, 
+        #                                     movies_slides = movies_slides,
+        #                                     tvshows_slides = tvshows_slides)
 
 
 if __name__ == '__main__':
