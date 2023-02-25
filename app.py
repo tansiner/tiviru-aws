da3873c8dfc1c163ea694c7e67cb3f79cc67522f
TAHEP <tansiner@hotmail.com>
2023-02-25 13:41:14 +0300

    commit

--- app.py
+++ app.py
@@ -59,45 +59,47 @@ class Media(db.Model):
 def home():
 
     
-    main_slider = Media
-    ms_slides = []
-    for slide in main_slider.query.filter_by().all():
-        ms_slides.append(slide)
+    # main_slider = Media
+    # ms_slides = []
+    # for slide in main_slider.query.filter_by().all():
+    #     ms_slides.append(slide)
 
   
-    series = Media
-    series_slides = []
-    for slide in series.query.filter_by(category="series", carousel="1").all():
+    # series = Media
+    # series_slides = []
+    # for slide in series.query.filter_by(category="series", carousel="1").all():
     
-        series_slides.append(slide)
+    #     series_slides.append(slide)
 
 
-    documentaries = Media
-    documentaries_slides = []
-    for slide in documentaries.query.filter_by(category="documentary", carousel="1").all():
+    # documentaries = Media
+    # documentaries_slides = []
+    # for slide in documentaries.query.filter_by(category="documentary", carousel="1").all():
     
-        documentaries_slides.append(slide)
+    #     documentaries_slides.append(slide)
 
 
-    movies = Media
-    movies_slides = []
-    for slide in movies.query.filter_by(category="movie", carousel="1").all():
+    # movies = Media
+    # movies_slides = []
+    # for slide in movies.query.filter_by(category="movie", carousel="1").all():
     
-        movies_slides.append(slide)
+    #     movies_slides.append(slide)
 
 
-    tvshows = Media
-    tvshows_slides = []
-    for slide in tvshows.query.filter_by(category="tvshow", carousel="1").all():
+    # tvshows = Media
+    # tvshows_slides = []
+    # for slide in tvshows.query.filter_by(category="tvshow", carousel="1").all():
     
-        tvshows_slides.append(slide)
+    #     tvshows_slides.append(slide)
 
 
-    return render_template("home.html", ms_slides = ms_slides,
-                                        series_slides = series_slides,
-                                        documentaries_slides = documentaries_slides, 
-                                        movies_slides = movies_slides,
-                                        tvshows_slides = tvshows_slides)
+    # return render_template("home.html", ms_slides = ms_slides,
+    #                                     series_slides = series_slides,
+    #                                     documentaries_slides = documentaries_slides, 
+    #                                     movies_slides = movies_slides,
+    #                                     tvshows_slides = tvshows_slides)
+
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
