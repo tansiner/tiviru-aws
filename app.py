27c48a66d781bf3b6e5f882308573636446c9d8e
TAHEP <tansiner@hotmail.com>
2023-02-25 13:44:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, escape, redirect, url_for, session, flash
+from flask import Flask, render_template, escape, redirect, url_for, session, flash, mysqlclient
 from flask_sqlalchemy import SQLAlchemy
 
 
