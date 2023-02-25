52c6825d7286d2a8cda73c12c90fa52bdbc308f2
TAHEP <tansiner@hotmail.com>
2023-02-25 13:34:51 +0300

    first commit

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, escape, json, jsonify, make_response, redirect, url_for, session, flash
+from flask import Flask, render_template, escape, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
 app.secret_key = "Secret Key"
 
 #SqlAlchemy Database Configuration With Mysql
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tiviru.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://vrkarqgzzzkmtn:0e32017efa344188261e0ca0fa2fe78a0c7f986a6604ce207a4921ca072f4b4e@ec2-34-246-155-237.eu-west-1.compute.amazonaws.com:5432/de51kvccijsfj2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin12345@database-1.cl7j5j2rhsoe.us-west-2.rds.amazonaws.com/flaskaws'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
 
 
 #Creating model table for Media
@@ -47,52 +49,61 @@ class Media(db.Model):
         self.main_slider = main_slider
         self.carousel = carousel
 
+
+
+    
+
+
 @app.route('/')
 @app.route('/home')
 def home():
+
     
-    # series = Media
-    # first_media = series.query.first()
+    main_slider = Media
+    ms_slides = []
+    for slide in main_slider.query.filter_by().all():
+        ms_slides.append(slide)
+
+  
+    series = Media
+    series_slides = []
+    for slide in series.query.filter_by(category="series", carousel="1").all():
     
-
-    # users = Users
-    # my_users = []
-    # for slide in users.query.filter_by().all():
-    #     my_users.append(slide)
+        series_slides.append(slide)
 
 
-    # series = Media
-    # series_slides = []
-    # for slide in series.query.filter_by(category="series", carousel="1").all():
+    documentaries = Media
+    documentaries_slides = []
+    for slide in documentaries.query.filter_by(category="documentary", carousel="1").all():
     
-    #     series_slides.append(slide)
+        documentaries_slides.append(slide)
 
 
-    # documentaries = Media
-    # documentaries_slides = []
-    # for slide in documentaries.query.filter_by(category="documentary", carousel="1").all():
+    movies = Media
+    movies_slides = []
+    for slide in movies.query.filter_by(category="movie", carousel="1").all():
     
-    #     documentaries_slides.append(slide)
+        movies_slides.append(slide)
 
 
-    # movies = Media
-    # movies_slides = []
-    # for slide in movies.query.filter_by(category="movie", carousel="1").all():
+    tvshows = Media
+    tvshows_slides = []
+    for slide in tvshows.query.filter_by(category="tvshow", carousel="1").all():
     
-    #     movies_slides.append(slide)
+        tvshows_slides.append(slide)
 
 
-    # tvshows = Media
-    # tvshows_slides = []
-    # for slide in tvshows.query.filter_by(category="tvshow", carousel="1").all():
-    
-    #     tvshows_slides.append(slide)
-
-
-    return render_template("home.html", first_data = "hello")
+    return render_template("home.html", ms_slides = ms_slides,
+                                        series_slides = series_slides,
+                                        documentaries_slides = documentaries_slides, 
+                                        movies_slides = movies_slides,
+                                        tvshows_slides = tvshows_slides)
 
 
 if __name__ == '__main__':
 
+    app.config['ENV'] = 'development'
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = False
 
-    app.run()
+    app.run(port=5001)
