59397a344ceaf7e4a60fa060bc71d3aebc1c640c
TAHEP <tansiner@hotmail.com>
2023-03-13 11:52:40 +0300

    update 1
    version 1

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template, escape, request, json, jsonify, make_response, redirect, url_for, render_template, session, flash
+from flask import Flask, flash, render_template, escape, request, json, jsonify, make_response, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+import urllib.request
+import os
+
+from werkzeug.utils import secure_filename
 from xml.dom import minidom
 from datetime import datetime
 
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.secret_key = "Secret Key"
 
+
+
+
+
 #SqlAlchemy Database Configuration With Mysql
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask-aws'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin12345@database-1.cl7j5j2rhsoe.us-west-2.rds.amazonaws.com/flaskaws'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flaskaws'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin12345@database-1.cl7j5j2rhsoe.us-west-2.rds.amazonaws.com/flaskaws'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
 
 db = SQLAlchemy(app)
 
@@ -55,9 +71,9 @@ class Media(db.Model):
     main_slider = db.Column(db.String(100))
     carousel = db.Column(db.String(100))
    
+    title,category,genres, summary, cast, img, source, country, status, year, main_slider, carousel
 
-
-    def __init__(self, id, title, category, genres, summary, cast, img, source, country, status, year, main_slider, carousel):
+    def __init__(self, id, title,category,genres, summary, cast, img, source, country, status, year, main_slider, carousel):
 
         self.id = id
         self.title = title
@@ -81,30 +97,48 @@ def home():
 
     main_slider = Media
     ms_slides = []
-    for slide in main_slider.query.filter_by(main_slider="1").all():
+    for slide in main_slider.query.filter_by(main_slider=1).all():
         ms_slides.append(slide)
 
     
     series = Media
     series_slides = []
-    for slide in series.query.filter_by(category="series", carousel="1").all():
+    for slide in series.query.filter_by(category="series", carousel=1).all():
         series_slides.append(slide)
+
+
+
+    uk_series = Media
+    uk_series_slides = []
+    for slide in series.query.filter_by(category="series", country="uk").all():
+        uk_series_slides.append(slide)
+
+
+    russian_series = Media
+    russian_series_slides = []
+    for slide in series.query.filter_by(category="series", country="ru").all():
+        russian_series_slides.append(slide)
+
+    usa_series = Media
+    usa_series_slides = []
+    for slide in series.query.filter_by(category="series", country="usa").all():
+        usa_series_slides.append(slide)
 
 
     documentaries = Media
     documentaries_slides = []
-    for slide in documentaries.query.filter_by(category="documentary", carousel="1").all():
+    for slide in documentaries.query.filter_by(category="documentary", carousel=1).all():
     
         documentaries_slides.append(slide)
 
     movies = Media
     movies_slides = []
-    for slide in movies.query.filter_by(category="movies", carousel="1").all():
+    for slide in movies.query.filter_by(category="movies", carousel=1).all():
     
         movies_slides.append(slide)
     
 
-    return render_template("home.html", ms_slides = ms_slides, series_slides = series_slides, documentaries_slides = documentaries_slides, movies_slides = movies_slides)
+    return render_template("home.html", ms_slides = ms_slides, series_slides = series_slides, uk_series = uk_series_slides, russian_series = russian_series_slides, usa_series = usa_series_slides, documentaries_slides = documentaries_slides, movies_slides = movies_slides)
 
 
 
@@ -250,10 +284,141 @@ def tv():
 
 
 
-@app.route('/tv2')
-def tv2():
 
-    return render_template("tv2.html")
+
+
+@app.route("/tv-edit", methods=["POST"])
+@app.route('/tv-edit')
+def tv_edit():
+
+    
+    if request.method == 'POST':
+
+        src = request.form['channel_src']
+        channel_id = request.form['channel_id']
+
+        print("channel id", channel_id)
+
+        # UPDATE
+        ch = Channels.query.filter_by(id=channel_id).first()
+        ch.webstream = src
+        db.session.commit()
+        
+
+    i = 0
+    index = 0
+
+    total_len = len(Channels.query.all())
+    current_epg = [[] for x in range(0,total_len)]
+    full_epg = [[] for x in range(0,total_len)]
+
+
+
+    from datetime import datetime
+
+    now = datetime.now()
+    
+
+    current_time = now.strftime("%H%M%S")
+    print("Current Time =", current_time)
+
+
+
+   
+
+
+    x = 0
+    channel_list_for_current_epg = []
+    channel_list_for_full_epg = []
+    
+    stream_option = "webstream"
+    
+   
+        
+    for channel in Channels.query:
+        
+        if channel.status == 1:
+
+            channel_list_for_current_epg.append(
+            {
+            'id': channel.id,
+            'channel_name': channel.channel_name,
+            'channel_logo': channel.channel_logo,
+            'webstream': channel.webstream,
+       
+            },)
+            
+
+
+            x += 1
+
+    return render_template("tv-edit.html", data=channel_list_for_current_epg)
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+    
+
+@app.route("/admin", methods=['GET', 'POST'])
+@app.route('/admin')
+def admin():
+
+
+    if request.method == 'POST':
+
+        
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+
+        file = request.files['file']
+        img = file.filename.rsplit('.', 1)
+        print(img[0])
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+        title = request.form['title']
+        category = request.form['category']
+        genres = request.form['genres']
+        summary = request.form['summary']
+        img = img[0]
+        cast = request.form['cast']
+        source = request.form['source']
+        country = request.form['country']
+        status = 1 if request.form.get('status') else 0
+        year = request.form['year']
+        main_slider = 1 if request.form.get('mainSlider') else 0
+        carousel = 1 if request.form.get('carousel') else 0
+
+
+
+
+        
+    
+
+        print(f"Category {category}")
+
+
+        my_data = Media(id, title,category,genres, summary, cast, img, source, country, status, year, main_slider, carousel)
+        db.session.add(my_data)
+        db.session.commit()
+
+    return render_template("admin.html")
+
+    
 
 
 
@@ -367,6 +532,54 @@ def update_schedule():
     return jsonify(channels_lst)
 
 
+
+
+
+
+
+
+
+
+
+
+@app.route("/form2", methods=['GET', 'POST'])
+@app.route('/form2')
+def form2():
+
+    
+    return render_template("form2.html")
+
+
+
+
+
+@app.route('/processFormDatas', methods = ['POST', 'GET'])
+def processFormDatas():
+    
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        category = request.form['category']
+        genres = request.form['genres']
+        summary = request.form['summary']
+        cast = request.form['cast']
+        img = request.form['img']
+        source = request.form['source']
+        country = request.form['country']
+        status = request.form['status']
+        year = request.form['year']
+        main_slider = request.form['main_slider']
+        carousel = request.form['carousel']
+        
+
+        print(title, category, genres, summary, cast, img, source, country, status, year, main_slider, carousel) 
+
+        my_data = Media(id, title, category, genres, summary, cast, img, source, country, status, year, main_slider, carousel)
+        db.session.add(my_data)
+        db.session.commit()       
+           
+
+    return jsonify("Your Content has been added successfully")
 
 
 
